661f635da7523b73b204358eb5081ccd7ea7872d
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 18:00:08 +0700

    sd

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -176,7 +176,11 @@ with st.form("triage_form"):
     #   chief_complaint 
     #   duration      
     # Variable names must be EXACTLY: chief_complaint, duration
-
+    col_cc, col_dur = st.columns(2)
+    with col_cc:
+        chief_complaint = st.selectbox("Chief Complaint", options=list(cc_map.keys()))
+    with col_dur:
+        duration = st.selectbox("Duration", options=list(dur_map.keys()))
 
     st.markdown("<br>", unsafe_allow_html=True)
 
