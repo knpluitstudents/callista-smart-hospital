6a9ee27890c8f79438a68b21f5f51e1bcf41566c
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 18:29:25 +0700

    rrgr

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -239,12 +239,12 @@ with st.form("triage_form"):
     # TODO (text/color): replace CODENO24 and COLOR6.
     st.markdown("""
     <!-- COLOR6 -->
-    <div style="background:#acf1e3;border:1px solid #39695b;border-radius:14px;
+    <div style="background:#ffffff;border:1px solid #1c1643;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#31703f;color:rgb(255, 255, 255);border-radius:8px;
+            <span style="background:#2c276c;color:rgb(255, 255, 255);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">5</span>
-            <span style="font-size:16px;font-weight:600;color:#ffffff;">Patient Information</span>
+            <span style="font-size:16px;font-weight:600;color:#000040;">Patient Information</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
