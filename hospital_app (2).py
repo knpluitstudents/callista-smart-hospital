c4b9db2f06e7242e40318be3ade68328a36d67af
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 17:42:40 +0700

    fwresfv

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -114,7 +114,7 @@ with st.form("triage_form"):
     # TODO (text/color): replace CODENO4 and COLOR2.
     st.markdown("""
     <!-- COLOR2 -->
-    <div style="background:#8ccce7;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#8ccce7;border:1px solid #4e48a5;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
             <span style="background:#2c80b4;color:white;border-radius:8px;
