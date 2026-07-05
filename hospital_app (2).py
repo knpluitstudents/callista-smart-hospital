229cd045a1f321869cd8cd35f4eaf5ebdcec1b92
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 17:53:20 +0700

    3rged

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -162,12 +162,12 @@ with st.form("triage_form"):
     # TODO (text/color): replace CODENO14 and COLOR3.
     st.markdown("""
     <!-- COLOR3 -->
-    <div style="background:#f60909;border:1px solid #cbdc0d;border-radius:14px;
+    <div style="background:#dde383;border:1px solid #cbdc0d;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:#25f20a;color:white;border-radius:8px;
+            <span style="background:hsl(157, 100%, 80%);color:rgb(164, 218, 213);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">2</span>
-            <span style="font-size:16px;font-weight:600;color:#105efa;">CODENO14</span>
+            <span style="font-size:16px;font-weight:600;color:#31473d;">How long have you had these symptoms?</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
