f0737031fab0584da8a08d770a1ce310a5dc4a2b
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 17:34:55 +0700

    update

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -37,7 +37,7 @@ div[data-testid="stCheckbox"] label {
 
 @st.cache_resource
 def load_model():
-    with open('hospital_model.pkl', 'rb') as f:
+    with open('hospital_model (1).pkl', 'rb') as f:
         return pickle.load(f)
 
 
