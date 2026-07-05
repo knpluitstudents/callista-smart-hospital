b1c8b3df641412ebe9fa7fead67a4d1056140a6c
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 17:55:13 +0700

    w

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -165,7 +165,7 @@ with st.form("triage_form"):
     <div style="background:#8ed3c0;border:1px solid #436157;border-radius:14px;
                 padding:20px 24px;margin-bottom:20px;">
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
-            <span style="background:hsl(157, 100%, 80%);color:rgb(255, 255, 255);border-radius:8px;
+            <span style="background:hsl(156, 19%, 35%);color:rgb(255, 255, 255);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">2</span>
             <span style="font-size:16px;font-weight:600;color:#ffffff;">How long have you had these symptoms?</span>
         </div>
