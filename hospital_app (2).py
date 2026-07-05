7345cece9d8ba0a89f35a53ba9b60aee7c851d85
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-05 18:04:39 +0700

    edsfcg

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -193,7 +193,7 @@ with st.form("triage_form"):
         <div style="display:flex;align-items:center;gap:10px;margin-bottom:4px;">
             <span style="background:#334c2f;color:rgb(114, 154, 83);border-radius:8px;
                          padding:4px 10px;font-size:12px;font-weight:600;">3</span>
-            <span style="font-size:16px;font-weight:600;color:#f6f9ff;">How would you rate the severity?</span>
+            <span style="font-size:16px;font-weight:600;color:#3b5324;">How would you rate the severity?</span>
         </div>
     </div>
     """, unsafe_allow_html=True)
