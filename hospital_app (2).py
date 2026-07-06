e392a5b69c735a3607e42fdd50c590a87bd77063
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-06 13:10:36 +0700

    def

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -262,6 +262,26 @@ with st.form("triage_form"):
 
 # ── Result ────────────────────────────────────────────────────────────────────
 if submitted:
+    patient = pd.DataFrame([{
+        'age': age,
+        'gender' : gender_map.get(gender,0),
+        'fever' : int(fever), 
+        'cough' : int(cough), 
+        'headache' : int(headache), 
+        'chest_pain' : int(chest_pain),
+        'stomach_pain' : int(stomach_pain),
+        'shortness_breath' : int(shortness_breath),
+        'nausea_vomitng' : int(nausea_vomiting),
+        'dizziness' : int(dizziness),
+        'skin_rash' : int(skin_rash),
+        'temperature_level' : temp_map.get(temperature_level, 1),
+        'heart_rate_level' : hr_map.get(heart_rate_level, 1),
+        'duration' : dur_map.get(duration, 1), 
+        'asthma' : int(asthma),
+        'hypertension' : int(hypertension),
+        'heart_disease' : int(heart_disease),
+        'chief_complaint' : cc_map.get(chief_complaint, 9)
+    }])
 
     # TODO (logic — core of Day 2): everything from here down to before
     # "st.markdown('---')" is for you to write. Steps:
