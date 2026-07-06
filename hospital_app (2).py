ef9464cf0c09c8a11b459e7b71c1ba249e665e05
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-06 13:31:56 +0700

    qwdc

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -359,6 +359,7 @@ if submitted:
             <div style="margin-top:20px;padding:12px 16px;background:rgba(0,0,0,0.05);
                         border-radius:10px;font-size:12px;color:#6b7280;line-height:1.5;">
                 ⚠️ This is an AI suggestion, not a medical diagnosis.
+                Please consult a Doctor for further evaluation
             </div>
         </div>
         """, unsafe_allow_html=True)
