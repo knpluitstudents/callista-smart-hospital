63d8440d981b4c65006187446363ac273f02d89f
knpluitstudents <kodingnextpluitstudents@gmail.com>
2026-07-06 13:02:47 +0700

    e

--- hospital_app (2).py
+++ hospital_app (2).py
@@ -258,7 +258,7 @@ with st.form("triage_form"):
         age = st.number_input("Age", min_value=1, max_value =100, value=30)
     with col_gen:
         gender = st.selectbox("Gender", options = ['Female', 'Male'])
-    submitted = st.form_submit_button("CODENO27 →")
+    submitted = st.form_submit_button("Get AI Recommendation →")
 
 # ── Result ────────────────────────────────────────────────────────────────────
 if submitted:
